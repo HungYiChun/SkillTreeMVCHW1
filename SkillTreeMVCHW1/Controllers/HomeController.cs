2094426e41f68431642412833a0026e6b19080a6
HungYiChun <white800612@gmail.com>
2018-01-19 10:15:06 +0800

    Skill Tree ASP.NET MVC Session1 Homework, First Commit

--- SkillTreeMVCHW1/Controllers/HomeController.cs
+++ SkillTreeMVCHW1/Controllers/HomeController.cs
@@ -1,18 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using SkillTreeMVCHW1.Services.Home;
 using System.Web.Mvc;
 
 namespace SkillTreeMVCHW1.Controllers
 {
     public class HomeController : Controller
     {
+        // Home Service
+        HomeService homeService = new HomeService();
+
+        // GET，Home，我的記帳本
         public ActionResult Index()
         {
             return View();
         }
 
+        // GET，[ChildActionOnly] Home/MoneyList，記帳資料
+        [ChildActionOnly]
+        public ActionResult MoneyList()
+        {            
+            return View(homeService.moneyList());
+        }
+
+        // GET，Home/About，關於
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -20,6 +29,7 @@ namespace SkillTreeMVCHW1.Controllers
             return View();
         }
 
+        // GET，Home/Contact，聯絡
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
